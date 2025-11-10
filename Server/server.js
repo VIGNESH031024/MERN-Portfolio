41fc830537d76375a94366833ecf9d5b074026f2
VignesVS <vsvigneshportfolio.com>
2025-11-10 19:25:14 +0530

    Many changes

--- Server/server.js
+++ Server/server.js
@@ -42,6 +42,6 @@ app.use("/api/education",educationRoutes);
 app.use("/api/messages", messageRoutes);
 
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-// export default app;
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+export default app;
