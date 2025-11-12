df5d22014721f7f37fe656254fe8e12dce96f397
VignesVS <vsvigneshportfolio.com>
2025-11-12 16:35:18 +0530

    comapss to atlas

--- Server/server.js
+++ Server/server.js
@@ -79,6 +79,8 @@ app.use("/api/education",educationRoutes);
 app.use("/api/messages", messageRoutes);
 
 
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-export default app;
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
