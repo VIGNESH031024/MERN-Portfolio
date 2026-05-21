c6efe919fda370daa3e82ec7a2b77c3716be089e
VIGNESH031024 <vigneshvignesh84569@gmail.com>
2026-05-21 23:32:38 +0530

    Fixed Vercel CORS issue

--- Server/server.js
+++ Server/server.js
@@ -24,30 +24,23 @@ const app = express();
 
 
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://your-actual-frontend.vercel.app",
-  "https://pd6l65fs-5173.inc1.devtunnels.ms"
-];
+// const allowedOrigins = [
+//   "http://localhost:5173",
+//   "https://your-actual-frontend.vercel.app",
+//   "https://pd6l65fs-5173.inc1.devtunnels.ms"
+// ];
 
 app.use(
   cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
-
-      return callback(new Error("Not allowed by CORS"));
-    },
+    origin: [
+      "http://localhost:5173",
+      "https://vignesh-vs.vercel.app",
+    ],
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
-app.options("*", cors());
+app.use(express.json());
 
 
 
